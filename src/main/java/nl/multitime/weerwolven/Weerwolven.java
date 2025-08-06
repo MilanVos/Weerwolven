5d7788b5f5fda3f89593b321020ef96cd812d209
MultiTime <64415590+MilanVos@users.noreply.github.com>
2025-08-06 23:09:03 +0200

    ...

--- src/main/java/nl/multitime/weerwolven/Weerwolven.java
+++ src/main/java/nl/multitime/weerwolven/Weerwolven.java
@@ -1,17 +1,43 @@
 package nl.multitime.weerwolven;
 
+import nl.multitime.weerwolven.commands.WeerwolvenCommand;
+import nl.multitime.weerwolven.game.GameManager;
+import nl.multitime.weerwolven.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Weerwolven extends JavaPlugin {
 
+    private static Weerwolven instance;
+    private GameManager gameManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        
+        this.gameManager = new GameManager(this);
+        
+        WeerwolvenCommand commandHandler = new WeerwolvenCommand(this);
+        getCommand("weerwolven").setExecutor(commandHandler);
+        getCommand("weerwolven").setTabCompleter(commandHandler);
+        
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        
+        getLogger().info("Weerwolven plugin enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (gameManager != null) {
+            gameManager.shutdown();
+        }
+        getLogger().info("Weerwolven plugin disabled!");
+    }
+    
+    public static Weerwolven getInstance() {
+        return instance;
+    }
+    
+    public GameManager getGameManager() {
+        return gameManager;
     }
 }
